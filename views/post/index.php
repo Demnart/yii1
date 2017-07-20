07e7bfe5d22271cc8e3f2088f71276730d5fe42d
genjo <insidia93@gmail.com>
2017-07-20 12:51:25 +0300

    Сделали красивый вывод сообщений о принятии или не принятии данных

--- views/post/index.php
+++ views/post/index.php
@@ -7,7 +7,21 @@ use yii\widgets\ActiveForm;
 <h1>Этот текст будет виден только на странице индекс</h1>
 <?php $this->endBlock();?>
 
-<?php $form = ActiveForm::begin(['id'=>'testForm']) ?>
+<?php if (Yii::$app->session->hasFlash('success')): ?>
+<div class="alert alert-success alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <?php echo Yii::$app->session->getFlash('success');?>
+</div>
+<?php endif; ?>
+<?php if (Yii::$app->session->hasFlash('error')): ?>
+    <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <?php echo Yii::$app->session->getFlash('error');?>
+    </div>
+<?php endif; ?>
+
+
+<?php $form = ActiveForm::begin(['options'=>['id'=>'testForm']]) ?>
 <?= $form->field($model,'name')?>
 <?= $form->field($model,'email')->input('email')?>
 <?= $form->field($model,'text')->textarea()?>
