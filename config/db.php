5c954cbe73f6a607036187ef6efd6edf38dca331
genjo <insidia93@gmail.com>
2017-07-21 13:35:58 +0300

    Создали подключение к нашей бд, прописали все данные для подключения

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2news',
     'username' => 'root',
-    'password' => '',
+    'password' => 'death9393',
     'charset' => 'utf8',
 ];
